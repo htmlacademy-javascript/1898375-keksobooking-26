ad1cf89a12c918c5958fca34a56913d7b542341a
Vitaliy Vasiliev <93582413+Ierixorn@users.noreply.github.com>
2022-06-16 17:49:44 +0300

    Создание author и location

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ function getRandomInt(min, max) {
   if (min >=0 && max > 0 && min !== max && max > min) {
     const minValue = Math.ceil(min);
     const maxValue = Math.floor(max);
+
     return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
   }
+
   return null;
 }
 
@@ -12,10 +14,71 @@ function getRandomInt(min, max) {
 function getRandomIntFloat(min, max, afterDecimal) {
   if (min >=0 && max > 0 && min !== max && max > min) {
     const intFloat = Math.random() * (max - min + 1) + min;
-    return intFloat.toFixed(afterDecimal);
+
+    return +intFloat.toFixed(afterDecimal);
   }
+
   return null;
 }
 
-getRandomInt(2,70);
-getRandomIntFloat(4,44,9);
+// avatar:
+function getNumbersArray(length) {
+  const someArray = [];
+
+  for (let i = 0; i <= length; i++) {
+    someArray[i] = String(i).padStart(2, 0);
+  }
+
+  return someArray;
+}
+
+const numbersArray = getNumbersArray(10);
+
+function getAvatarNumber () {
+  const randomIndex = getRandomInt (1, numbersArray.length - 1);
+  return numbersArray[randomIndex];
+}
+
+const avatarNumber = getAvatarNumber();
+
+
+// function createOffer () {
+//   return {
+//     title: ,
+//     address: ,
+//     price: ,
+//     type: ,
+//     rooms: ,
+//     guests: ,
+//     checkin: ,
+//     checkout: ,
+//     features: ,
+//     photos:
+//   }
+
+// }
+
+function getAuthor () {
+  return {
+    avatar: `img/avatars/user${avatarNumber}.png`,
+  };
+}
+
+function getLocation () {
+  const latNumber = getRandomIntFloat(35.65000, 35.70000, 5);
+  const lngNumber = getRandomIntFloat(139.70000, 139.80000, 5);
+  return {
+    lat: latNumber,
+    lng: lngNumber
+  };
+}
+
+function createObject () {
+
+  return {
+    author: getAuthor(),
+    location: getLocation()
+  };
+}
+
+console.log(createObject());
